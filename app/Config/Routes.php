1411f678d8ff13eefccca6e14af01f9cd870449a
Ialy Vestalys <ialy.vest@gmail.com>
2026-05-08 23:14:34 +0300

    création des routes pour l'inscription

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,9 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+// Inscription en 2 étapes
+$routes->get('/register',        'AuthController::register');
+$routes->post('/register/step1', 'AuthController::registerStep1Post');
+$routes->get('/register/step2',  'AuthController::registerStep2');
+$routes->post('/register/step2', 'AuthController::registerStep2Post');
